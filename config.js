476560b4c096071be04ebfb9748d865c12870f74
Swapnil Deshpande <dswap123@gmail.com>
2018-08-15 20:29:33 +0530

    changed config file to use all specs

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ exports.config = {
     'browserName': 'chrome'
     },
   framework: 'jasmine',
-  specs: ['./specs/deleteList.js'],
-  //  specs: ['./specs/addList.js','./specs/validateElements.js','./specs/deleteList.js'],
+//  specs: ['./specs/deleteList.js'],
+  specs: ['./specs/addList.js','./specs/validateElements.js','./specs/deleteList.js'],
   jasmineNodeOpts: {
     defaultTimeoutInterval: 30000
   }
